7b41be55df2da695688e9ee8b03b3d86748e937a
OM SONANI <143808904+om-151@users.noreply.github.com>
2025-01-28 18:13:37 +0530

    set

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const paymentRoute = require("./routes/payment-routes")
 const app = express()
 const port = process.env.PORT || 3000
 
-const allowedOrigins = ['https://frontend-p6xn.onrender.com/undefined'];
+const allowedOrigins = ['https://frontend-p6xn.onrender.com'];
 
 const corsOptions = {
     origin: (origin, callback) => {
